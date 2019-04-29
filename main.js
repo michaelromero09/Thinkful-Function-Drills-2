0b3102daeafb3f548e7cc162d113108c401d81b1
AstridFlare <AstridFlare@Gmail.com>
2019-04-29 13:51:49 -0700

    beyond()

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-'use strict'
+"use strict";
 
 function jediName(firstName, lastName) {
-  let name = '';
-  name = lastName.slice(0,3) + firstName.slice(0, 2);
+  let name = "";
+  name = lastName.slice(0, 3) + firstName.slice(0, 2);
   return name;
 }
 
-let myJediName = jediName('Michael', 'Romero');
-console.log(myJediName);
+let myJediName = jediName("Michael", "Romero");
+// console.log(myJediName);
+
+function beyond(num) {
+  if (num === Number.POSITIVE_INFINITY || num === Number.NEGATIVE_INFINITY) {
+    console.log("And Beyond!");
+  } else if (num > 0) {
+    console.log("To Infinity!");
+  } else if (num < 0) {
+    console.log("To Negative Infinity!");
+  } else if (num === 0) {
+    console.log("Staying Home");
+  } else {
+    console.log("Not an integer!");
+  }
+}
+
+beyond(Number.NEGATIVE_INFINITY);
